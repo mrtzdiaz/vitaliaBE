6d0a0b3d47ac025686b91096146e38e08aae56b7
jesus <jesus.hdz2599@gmail.com>
2024-07-03 16:23:38 -0600

    Se agrega clase pedido

---
+++ src/main/java/com/vitalia/vitaliaBE/model/pedido.java
@@ -0,0 +1,70 @@
+package com.vitalia.vitaliaBE.model;
+
+import java.util.Date;
+
+public class pedido {
+	
+	private Long id;
+	private String status;
+	private Date fechaPedido;
+	private Date fechaentrega;
+	private String formaDePago;
+	
+	public pedido() {
+		
+	}
+	
+	public pedido(Long id, String status, Date fechaPedido, Date fechaentrega, String formaDePago) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.fechaPedido = fechaPedido;
+		this.fechaentrega = fechaentrega;
+		this.formaDePago = formaDePago;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getFechaPedido() {
+		return fechaPedido;
+	}
+
+	public void setFechaPedido(Date fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+
+	public Date getFechaentrega() {
+		return fechaentrega;
+	}
+
+	public void setFechaentrega(Date fechaentrega) {
+		this.fechaentrega = fechaentrega;
+	}
+
+	public String getFormaDePago() {
+		return formaDePago;
+	}
+
+	public void setFormaDePago(String formaDePago) {
+		this.formaDePago = formaDePago;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "pedido [id=" + id + ", status=" + status + ", fechaPedido=" + fechaPedido + ", fechaentrega="
+				+ fechaentrega + ", formaDePago=" + formaDePago + "]";
+	}
+	
+	
+
+}
