e3d0002ee22713ce0f4923ec8b875165674f3891
Daniel <mrtzdiazdaniel@gmail.com>
2024-07-03 12:17:07 -0600

    Archivos springboot

---
+++ src/main/java/com/vitalia/vitaliaBE/VitaliaBeApplication.java
@@ -0,0 +1,13 @@
+package com.vitalia.vitaliaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaliaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaliaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitalia/vitaliaBE/VitaliaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalia.vitaliaBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaliaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
