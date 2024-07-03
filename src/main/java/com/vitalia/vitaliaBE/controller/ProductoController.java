2957d11dcc4a257703db15f6199450ca1548c555
Brandon Bazan <bjbaz.mur@gmail.com>
2024-07-03 17:03:24 -0600

    Se agregaron la clase Producto y el Producto Controller

---
+++ src/main/java/com/vitalia/vitaliaBE/controller/ProductoController.java
@@ -0,0 +1,52 @@
+package com.vitalia.vitaliaBE.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vitalia.vitaliaBE.model.Producto;
+
+@RestController
+@RequestMapping(path="/api/productos/")
+public class ProductoController {
+	
+	@GetMapping
+	public ArrayList<Producto> getProducts(){
+		return null;
+	}
+	
+	@GetMapping(path = "{ProductoId}")
+	public Producto getProductById(@PathVariable("ProductoId") Long id){
+		return null;
+	}
+	
+	@DeleteMapping(path = "{ProductoId}")
+	public Producto deleteProductById(@PathVariable("ProductoId") Long id){
+		return null;
+	}
+	
+	@PostMapping
+	public Producto addProduct(@RequestBody Producto producto) {
+		return null;
+	}
+	
+	@PutMapping(path = "{ProductoId}")
+	public Producto updateProduct(@PathVariable("ProductoId") Long id,
+			@RequestParam(required=false) String nombre,
+			@RequestParam(required=false) String descripcion,
+			@RequestParam(required=false) String imagen,
+			@RequestParam(required=false) Double precio,
+			@RequestParam(required=false) String categoria
+			) {
+	return null;
+	}
+
+}
