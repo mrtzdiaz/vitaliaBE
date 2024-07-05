c988d6997328638e88d3f9deea8935ff5b31ee60
diegohernandezr <diegohernandez.rodriguez1994@gmail.com>
2024-07-05 13:32:26 -0600

    CategoriasRepository

---
+++ src/main/java/com/vitalia/vitaliaBE/repository/CategoriasRepository.java
@@ -0,0 +1,13 @@
+package com.vitalia.vitaliaBE.repository;
+import java.util.Optional;
+
+
+import com.vitalia.vitaliaBE.model.Categorias;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriasRepository extends JpaRepository<Categorias, Long>{
+	Optional<Categorias>findByNombre(String Nombre);
+	Optional<Categorias>findByDescripcion(String Descripcion);
+}
