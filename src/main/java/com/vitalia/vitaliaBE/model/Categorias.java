89d8b368254c7633f1ae9006d621a6f468c68d66
jesus <jesus.hdz2599@gmail.com>
2024-07-04 20:53:47 -0600

    cambios de categoria controller

--- src/main/java/com/vitalia/vitaliaBE/model/Categorias.java
+++ src/main/java/com/vitalia/vitaliaBE/model/Categorias.java
@@ -1,16 +1,7 @@
 package com.vitalia.vitaliaBE.model;
 
 public class Categorias {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		private static int total=0;
-		private int id;
-=======
-	
->>>>>>> jesus
-=======
 
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 		private String nombre;
 		private String Tipodeproducto;
 		
@@ -23,58 +14,25 @@ public class Categorias {
 		public Categorias() {
 			Categorias.total++;
 			this.id=total;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-			
->>>>>>> jesus
-=======
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 		}//constructor vacio
 		
 		
 		//constructor
 		public Categorias(String nombre, String Tipodeproducto) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-			this.nombre = nombre;
-			this.Tipodeproducto = Tipodeproducto;
-			
-			Categorias.total++;
-			this.id=total;
-		
-=======
-=======
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
+
+	
 			super();
 			this.nombre = nombre;
 			this.Tipodeproducto = Tipodeproducto;
 			Categorias.total++;
 			this.id=total;
-<<<<<<< HEAD
->>>>>>> jesus
 		}//constructor
 
-		
-		
-<<<<<<< HEAD
-=======
-		}//constructor
-
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 		//Gett & setters
 		public int getId() {
 			return id;
 		}
 
-<<<<<<< HEAD
-
-=======
-	
-		
->>>>>>> jesus
-=======
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 		public String getNombre() {
 			return nombre;
 		}
@@ -84,26 +42,10 @@ public class Categorias {
 			this.nombre = nombre;
 		}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> jesus
-=======
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 		public String getTipodeproducto() {
 			return Tipodeproducto;
 		}
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-		public void setTipodeproducto(String Tipodeproducto) {
-			this.Tipodeproducto = Tipodeproducto;
-		}//Gett & setters
-=======
->>>>>>> jesus
-=======
->>>>>>> 8407092e53b95f6a51ff262a3f253683ce48ca6b
 
 		public void setTipodeproducto(String tipodeproducto) {
 			Tipodeproducto = tipodeproducto;
