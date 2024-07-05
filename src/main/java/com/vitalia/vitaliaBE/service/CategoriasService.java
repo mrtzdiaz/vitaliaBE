70be50101933eb4c2bfad442dad292392ddf018a
diegohernandezr <diegohernandez.rodriguez1994@gmail.com>
2024-07-05 15:36:13 -0600

    Categorias service

--- src/main/java/com/vitalia/vitaliaBE/service/CategoriasService.java
+++ src/main/java/com/vitalia/vitaliaBE/service/CategoriasService.java
@@ -1,9 +1,13 @@
 package com.vitalia.vitaliaBE.service;
 
-import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.vitalia.vitaliaBE.model.Categorias;
+import com.vitalia.vitaliaBE.repository.CategoriasRepository;
+
 
 
 
@@ -12,70 +16,58 @@ import com.vitalia.vitaliaBE.model.Categorias;
 
 @Service
 public class CategoriasService {
-	private static ArrayList<Categorias> listaCategorias= new ArrayList<Categorias>();
+	private final CategoriasRepository categoriasRepository;
 	
 	@Autowired
-	public CategoriasService(ArrayList<Categorias> listaCategorias) {
-		this.listaCategorias = listaCategorias;
-		listaCategorias.add(new Categorias("Shampoo","Cuidado personal"));
-		listaCategorias.add(new Categorias("Bebida","Suplementos alimenticios"));
-		listaCategorias.add(new Categorias("Jarabe","Medicamento Homeópata"));
+	public CategoriasService(CategoriasRepository categoriasRepository) {
+		this.categoriasRepository = categoriasRepository;
 	}
 	
-	public ArrayList<Categorias> getListaCategorias(){
-		return listaCategorias;
+	public List<Categorias> getAllCategories(){
+		return categoriasRepository.findAll();
 	}
 	
-	public Categorias getCategorias(int id) {
+	public Categorias getCategories(Long id) {
+		return categoriasRepository.findById(id).orElseThrow(
+				()-> new IllegalArgumentException("La Categoria con el Id ["+
+						id+" ] no existe")
+				);
+	}//getCategorias
+	
+		
+	
+	public Categorias deleteCategorias(Long id) {
 		Categorias tmpcat = null;
-		for (Categorias categorias : listaCategorias) {
-			if (categorias.getId()==id) {
-				tmpcat = categorias;
-				break;
-			}
-		}
-		return tmpcat;
-	}
-	
-	public Categorias deleteCategorias(int id) {
-		Categorias tmpcat = null;
-		for (Categorias categorias : listaCategorias) {
-			if (categorias.getId()==id) {
-				tmpcat = listaCategorias.remove(listaCategorias.indexOf(categorias));
-				break;
-			}
-		}
-		return tmpcat;
-	}
+		if(categoriasRepository.existsById(id)) {
+			tmpcat=categoriasRepository.findById(id).get();
+			categoriasRepository.deleteById(id);			
+		}//if
+		return tmpcat;		
+	}//deleteProduct
 	
 	public  Categorias addCategories(Categorias categorias) {
-		Categorias tmpcat=null;
-		boolean existe=false;
-		for (Categorias cat : listaCategorias) {
-			if(cat.getNombre().equals(categorias.getNombre())) {
-				existe=true;		
-				break;
-			}//if
-		}//for
-		if(!existe) {
-			listaCategorias.add(categorias);
-			tmpcat=categorias;
-		}//if
-		return tmpcat;
-	}//addProduct
+		Optional <Categorias> tmpcat = categoriasRepository.findByNombre(categorias.getNombre());
+		if(tmpcat.isEmpty()) {
+			return categoriasRepository.save(categorias);
+		}else {
+			System.out.println("La categoria ya existe ya existe");
+			return null;
+		}	
+	}//addCategories
 	
-	public Categorias updateCategorias(int id, String nombre, String Tipodeproducto) {
-		Categorias cat = null;
-		for (Categorias categorias : listaCategorias) {
-			if (categorias.getId()==id) {
-				if(nombre !=null) categorias.setNombre(nombre);;
-				if(Tipodeproducto !=null) categorias.setTipodeproducto(Tipodeproducto);;
-				cat = categorias;
-				break;
-			}
-		}
-		return cat;
-	}
+	public Categorias updateCategorias(long id, String nombre, String Tipodeproducto) {
+		Categorias tmpcat = null;
+		
+		if(categoriasRepository.existsById(id)) {
+			Categorias categorias = categoriasRepository.findById(id).get();
+			if(nombre!=null)categorias.setNombre(nombre);
+			if(Tipodeproducto!=null)categorias.setTipodeproducto(Tipodeproducto);;
+	
+			categoriasRepository.save(categorias);
+			tmpcat=categorias;
+		}		
+	return tmpcat;
+}
 	
 
 		
