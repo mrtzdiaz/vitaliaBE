9bd5e65376d2cb62f1108ebb79698893b4154cde
Daniel <mrtzdiazdaniel@gmail.com>
2024-07-11 01:22:23 -0600

    Correccion de errores

--- src/main/java/com/vitalia/vitaliaBE/controller/LoginController.java
+++ src/main/java/com/vitalia/vitaliaBE/controller/LoginController.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import javax.servlet.ServletException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,29 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vitalia.vitaliaBE.config.JwtFilter;
 import com.vitalia.vitaliaBE.dto.Token;
 import com.vitalia.vitaliaBE.model.Usuario;
+import com.vitalia.vitaliaBE.repository.UsuarioRepository;
 import com.vitalia.vitaliaBE.service.UsuarioService;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+@CrossOrigin(origins="http://127.0.0.1:3003")
 @RestController
 @RequestMapping(path="/api/login/")
 public class LoginController {
 	private final UsuarioService usuarioService;
+	private final UsuarioRepository usuarioRepository;
 	@Autowired
-	public LoginController(UsuarioService usuarioService) {
+	public LoginController(UsuarioService usuarioService, UsuarioRepository usuarioRepository) {
 		this.usuarioService = usuarioService;
+		this.usuarioRepository = usuarioRepository;
 	}
 
 	@PostMapping
 	public Token loginUser(@RequestBody Usuario usuario) 
 										throws ServletException {
 		if (usuarioService.validateUser(usuario)) {
+			Usuario user = usuarioRepository.findByCorreo(usuario.getCorreo()).get();
 			System.out.println("Usuario válido " + usuario.getCorreo());
-			return new Token(generateToken(usuario.getCorreo()));
-		}//if
-		throw new ServletException("NOmbre de usuario o contraseña incorrectos [" 
-		+ usuario.getCorreo()+ "]");
+			return new Token(generateToken(usuario.getCorreo()), user.getNombre());
+		}
+		throw new ServletException("Nombre de usuario o contraseña incorrectos [" + usuario.getCorreo()+ "]");
 	}//login
 	
 	private String generateToken(String username) {
--- src/main/java/com/vitalia/vitaliaBE/controller/UsuarioController.java
+++ src/main/java/com/vitalia/vitaliaBE/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.vitalia.vitaliaBE.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.vitalia.vitaliaBE.dto.ChangePassword;
 import com.vitalia.vitaliaBE.model.Usuario;
 import com.vitalia.vitaliaBE.service.UsuarioService;
 
+@CrossOrigin(origins="http://127.0.0.1:3003")
 @RestController
 @RequestMapping(path="/api/usuarios/")
 public class UsuarioController {
